16a24023db02242468c62f9928bf7a96de3ab18a
Tyrus Tenneson <ttenneson@gmail.com>
2021-02-01 12:19:26 -0800

    Some emacs cleanup.

--- home/.emacs.d/ttt-lisp/custom.el
+++ home/.emacs.d/ttt-lisp/custom.el
@@ -3,19 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
+ '(ansi-color-names-vector
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
  '(background-color "#002b36")
  '(background-mode dark)
- '(browse-url-browser-function (quote browse-url-generic))
+ '(browse-url-browser-function 'browse-url-generic)
  '(browse-url-generic-program "/usr/bin/google-chrome")
  '(cursor-color "#839496")
- '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(custom-safe-themes
+   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))
  '(foreground-color "#839496")
- '(show-trailing-whitespace t)
- '(tab-width 4))
+ '(package-selected-packages
+   '(eglot-jl company python-black unicode-fonts flymake-diagnostic-at-point eglot elpy yaml-mode solidity-mode smart-mode-line persp-mode markdown-mode jedi irony-eldoc ido-completing-read+ flycheck-ycmd flycheck-irony exec-path-from-shell company-ycmd company-irony company-go company-c-headers clang-format))
+ '(python-shell-interpreter "python3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(company-preview ((t (:background "(face-attribute 'company-preview-common :background)"))))
+ '(popup-tip-face ((t (:background "#eee8d5")))))
